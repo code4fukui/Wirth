d7f84ce0936745102b9572aa84e0047ffa092106
Taisuke Fukuno <fukuno@jig.jp>
2024-12-15 17:15:54 +0900

    change ext from dncl3 to dncl

--- DNCL3.example.js
+++ DNCL3.example.js
@@ -1,7 +1,7 @@
 import { DNCL3 } from "./DNCL3.js";
 
 const name = Deno.args[0] ?? "test";
-const fn = "examples/" + name + ".dncl3";
+const fn = "examples/" + name + ".dncl";
 
 const s = await Deno.readTextFile(fn);
 new DNCL3(s).run();
